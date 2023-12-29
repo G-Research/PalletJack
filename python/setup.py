0f09ce0cc891be3f6580904d8d54236dd85ee099
Marcin Krystianc <marcin.krystianc@gmail.com>
2023-12-29 15:11:33 +0100

    Run tests on all OSs (#4)

--- python/setup.py
+++ python/setup.py
@@ -56,6 +56,7 @@ extensions = [
         library_dirs = pyarrow.get_library_dirs(),
         libraries=["arrow", "parquet"], 
         language = "c++",
+        extra_compile_args = ['/std:c++20'] if sys.platform.startswith('win') else ['-std=c++20']
     )
 ]
 
