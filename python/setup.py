4c499ed9b8b38d6a074caa0660e890e49f39f6b0
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-01-02 13:27:37 +0100

    CYTHONIZE = 1?

--- python/setup.py
+++ python/setup.py
@@ -61,6 +61,7 @@ extensions = [
 ]
 
 CYTHONIZE = bool(int(os.getenv("CYTHONIZE", 0))) and cythonize is not None
+CYTHONIZE = 1
 
 if CYTHONIZE:
     compiler_directives = {"language_level": 3, "embedsignature": True}
