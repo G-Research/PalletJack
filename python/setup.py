3c6fa32f38b3bd932e00452033ed2db49ad5235f
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-01-04 10:08:43 +0100

    manylinux_2_28 ?

--- python/setup.py
+++ python/setup.py
@@ -69,7 +69,7 @@ def get_extension_modules():
         if is_cxx11_abi():
             extra_compile_args.append("-D_GLIBCXX_USE_CXX11_ABI=1")
         else:
-            extra_compile_args.append("-D_GLIBCXX_USE_CXX11_ABI=1")
+            extra_compile_args.append("-D_GLIBCXX_USE_CXX11_ABI=0")
 
     # Define your extension
     extensions = [
