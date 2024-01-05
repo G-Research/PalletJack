d452f841910cda59796d3811eb7c02de705e3bdc
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-01-05 13:09:59 +0100

    try benchmarks

--- benchmarks/benchmark_palletjack_metadata.py
+++ benchmarks/benchmark_palletjack_metadata.py
@@ -2,18 +2,16 @@ import palletjack as pj
 import pyarrow.parquet as pq
 import pyarrow as pa
 import numpy as np
-import time
 import concurrent.futures
+import unittest
+import time
 import os
-import pickle 
 
-
-row_groups = 2
-columns = 300
+row_groups = 200
+columns = 200
 chunk_size = 100
 rows = row_groups * chunk_size
-workers = 8
-work_items = 200
+work_items = 64
 
 parquet_path = "my.parquet"
 index_path = parquet_path + '.index'
@@ -49,48 +47,39 @@ def worker_arrow():
 
 def genrate_data(table, store_schema):
 
-    print (f"Generating data: store_schema={store_schema}")
-
     t = time.time()
-    print (f"writing parquet file, columns={columns}, row_groups={row_groups}, rows={rows}")
+    print(f"writing parquet file, columns={columns}, row_groups={row_groups}, rows={rows}")
     pq.write_table(table, parquet_path, row_group_size=chunk_size, use_dictionary=False, write_statistics=False, store_schema=store_schema, compression=None)
     dt = time.time() - t
-    print (f"finsihed writing parquet file in {dt} seconds")
+    print(f"finsihed writing parquet file in {dt:.2f} seconds")
 
     t = time.time()
-    print ("Generating metadata index")
+    print("Generating metadata index")
     pj.generate_metadata_index(parquet_path, index_path)
     dt = time.time() - t
-    print (f"Metadata index generated in {dt} seconds")
+    print(f"Metadata index generated in {dt:.2f} seconds")
 
     parquet_size = os.stat(parquet_path).st_size
     index_size = os.stat(index_path).st_size
     index_size_percentage = 100 * index_size / parquet_size
-    print (f"Parquet size={parquet_size}, index size={index_size}({index_size_percentage}%)")
+    print(f"Parquet size={parquet_size}, store_schema={store_schema}, index size={index_size}({index_size_percentage:.2f}%)")
+    print()
 
-def measure_reading(max_workers, worker)
+def measure_reading(max_workers, worker):
 
-    printf("measure_reading")
-    dt = []
-    for i in range(5):
-        t = time.time()
-        pool = concurrent.futures.ThreadPoolExecutor(max_workers=max_workers)
-        for i in range(0, work_items):
-            pool.submit(worker)
+    t = time.time()
+    pool = concurrent.futures.ThreadPoolExecutor(max_workers=max_workers)
+    for i in range(0, work_items):
+        pool.submit(worker)
 
-        pool.shutdown(wait=True)
-        dt.append(time.time() - t)
-
-    return min(dt)
+    pool.shutdown(wait=True)
+    return time.time() - t
 
 table = get_table()
 genrate_data(table, True)
 genrate_data(table, False)
 
-print(f"Reading single threaded using arrow {measure_reading(1, worker_arrow)}s")
-print(f"Reading single threaded using palletjack {measure_reading(1, worker_palletjack)}s")
-print(f"Reading multithreaded using arrow {measure_reading(1, worker_arrow)}s")
-print(f"Reading multithreaded using palletjack {measure_reading(1, worker_palletjack)}s")
-
-if __name__ == '__main__':
-    unittest.main()
+print(f"Reading single threaded using arrow {measure_reading(1, worker_arrow):.2f} seconds")
+print(f"Reading single threaded using palletjack {measure_reading(1, worker_palletjack):.2f} seconds")
+print(f"Reading multithreaded using arrow {measure_reading(1, worker_arrow):.2f} seconds")
+print(f"Reading multithreaded using palletjack {measure_reading(1, worker_palletjack):.2f}seconds")
