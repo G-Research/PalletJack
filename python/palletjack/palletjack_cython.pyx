e0e6f3a979d0045e215f02770ba5d4c0c9f7c34c
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-01-17 22:38:05 +0100

    reuse ReadRowGroupsMetadata

--- python/palletjack/palletjack_cython.pyx
+++ python/palletjack/palletjack_cython.pyx
@@ -13,17 +13,7 @@ def generate_metadata_index(parquet_path, index_file_path):
     cpalletjack.GenerateMetadataIndex(parquet_path.encode('utf8'), index_file_path.encode('utf8'))
 
 cpdef read_row_group_metadata(index_file_path, row_group):
-
-    cdef shared_ptr[CFileMetaData] c_metadata
-    cdef string encoded_path = index_file_path.encode('utf8')
-    cdef uint32_t crow_group = row_group
-    with nogil:
-        c_metadata = cpalletjack.ReadRowGroupMetadata(encoded_path.c_str(), crow_group)
-
-    # Can we jsut create our own copy of file metadata ? It is a python, so no one cares if we reurn another type of object 
-    cdef FileMetaData m = FileMetaData.__new__(FileMetaData)
-    m.init(c_metadata)
-    return m
+    return read_row_groups_metadata(index_file_path, [row_group])
 
 cpdef read_row_groups_metadata(index_file_path, row_groups):
 
