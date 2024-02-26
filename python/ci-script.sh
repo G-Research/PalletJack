d651eb0fa4728afd26e422b33c99c46bef60a664
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-02-26 22:42:58 +0100

    --with-qt5=no

--- python/ci-script.sh
+++ python/ci-script.sh
@@ -1,14 +1,9 @@
 
 yum -y install libevent-devel zlib-devel openssl-devel
-wget http://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.tar.gz
-tar xvf boost_1_56_0.tar.gz
-cd boost_1_56_0
-./bootstrap.sh
-./b2 install
 git clone https://github.com/apache/thrift.git
 cd thrift
 ./bootstrap.sh
 cat configure
-./configure --with-lua=no --with-qt5=no
+./configure --with-qt5=no
 make
 make install
