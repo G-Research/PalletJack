d2c2a0cb70deca301d0f88c09e251d24e925f210
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-02-26 23:09:23 +0100

    cp

--- python/ci-script.sh
+++ python/ci-script.sh
@@ -3,6 +3,7 @@ yum -y install libevent-devel zlib-devel openssl-devel pkgconf
 yum -y install pkg-conf
 git clone https://github.com/apache/thrift.git
 cd thrift
+cp /usr/share/aclocal/pkg.m4 aclocal
 ./bootstrap.sh
 ./configure --without-qt5
 make
