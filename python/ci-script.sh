7c0074b4a086edac8b116c413935ff3f3f1031d9
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-02-26 22:50:21 +0100

    q

--- python/ci-script.sh
+++ python/ci-script.sh
@@ -2,8 +2,7 @@
 yum -y install libevent-devel zlib-devel openssl-devel
 git clone https://github.com/apache/thrift.git
 cd thrift
-./bootstrap.sh
-cat configure
-./configure --without-qt5
+./bootstrap.sh  --without-qt5
+./configure
 make
 make install
