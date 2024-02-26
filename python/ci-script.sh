0418530359fca6fb8d6f041542347b027d56039b
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-02-26 17:30:55 +0100

    reduce?

--- python/ci-script.sh
+++ python/ci-script.sh
@@ -1,32 +1,3 @@
-yum -y update
-
-yum -y groupinstall "Development Tools"
-
-yum install -y wget
-
-wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
-tar xvf autoconf-2.69.tar.gz
-cd autoconf-2.69
-./configure --prefix=/usr
-make
-make install
-cd ..
-
-wget http://ftp.gnu.org/gnu/automake/automake-1.14.tar.gz
-tar xvf automake-1.14.tar.gz
-cd automake-1.14
-./configure --prefix=/usr
-make
-make install
-cd ..
-
-wget http://ftp.gnu.org/gnu/bison/bison-2.5.1.tar.gz
-tar xvf bison-2.5.1.tar.gz
-cd bison-2.5.1
-./configure --prefix=/usr
-make
-make install
-cd ..
 
 yum -y install libevent-devel zlib-devel openssl-devel
 wget http://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.tar.gz
