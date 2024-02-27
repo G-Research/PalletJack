f276d9ccda436f01cadbda2aac3c573527e64a35
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-02-27 12:02:49 +0100

    ci

--- python/ci-script.sh
+++ python/ci-script.sh
@@ -1,19 +1,15 @@
 cat /etc/os-release
-lsb_release -a
-cat /etc/issue
 uname -a
 
-yum -y update
 yum -y groupinstall "Development Tools"
-
-yum -y install libevent-devel zlib-devel openssl-devel 
 yum -y install flex
 yum -y install boost boost-thread boost-devel
+yum -y install libevent-devel zlib-devel openssl-devel 
 git clone https://github.com/apache/thrift.git
 cd thrift
 cp /usr/share/aclocal/pkg.m4 aclocal
 ./bootstrap.sh
-./configure --with-cpp=yes
+./configure
 make
 make install
 find /usr -depth -name "*thrift*"
