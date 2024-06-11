4c024cf5c0ac0a245b49e75fda2284012986174b
Marcin Krystianc <marcin.krystianc@gmail.com>
2024-06-11 14:07:04 +0200

    Complete the migration to the G-Research org (#57)

--- python/setup.py
+++ python/setup.py
@@ -96,7 +96,7 @@ setup(
     ext_modules=extensions,
     test_suite = 'test',
     project_urls={
-        "Documentation": "https://github.com/marcin-krystianc/PalletJack",
-        "Source": "https://github.com/marcin-krystianc/PalletJack",
+        "Documentation": "https://github.com/G-Research/PalletJack",
+        "Source": "https://github.com/G-Research/PalletJack",
     },
 )
