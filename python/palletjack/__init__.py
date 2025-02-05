eee04d37b7963ab16f76bdd28c2c54a80234b855
Marcin Krystianc <marcin.krystianc@gmail.com>
2025-02-05 09:11:06 +0100

    Explicit errors message when arrow version mismatch (#80)

--- python/palletjack/__init__.py
+++ python/palletjack/__init__.py
@@ -1,3 +1,17 @@
 # Importing pyarrow is necessary to load the runtime libraries
+import pyarrow
 import pyarrow.parquet
-from .palletjack_cython import *
+
+from .package_metadata import (
+    __version__,
+    __dependencies__
+)
+
+try:
+    from .palletjack_cython import *
+except ImportError as e:
+    if any(x in str(e) for x in ['arrow', 'parquet']):
+        pyarrow_req = next((r for r in __dependencies__ if r.startswith('pyarrow')), '')
+        raise ImportError(f"This version of {__package__}={__version__} is built against {pyarrow_req}, please ensure you have it installed. Current pyarrow version is {pyarrow.__version__}. ({str(e)})") from None
+    else:
+        raise
