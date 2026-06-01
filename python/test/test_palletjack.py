419467e8e91e619397fd3007863f8eb0406f3bda
Marcin Krystianc <marcin.krystianc@gmail.com>
2026-06-01 22:42:46 +0200

    tests

--- python/test/test_palletjack.py
+++ python/test/test_palletjack.py
@@ -1,8 +1,10 @@
 import unittest
 import tempfile
+import base64
 
 import palletjack as pj
 import pyarrow.parquet as pq
+import pyarrow.parquet.encryption as pe
 import pyarrow as pa
 import numpy as np
 import itertools as it
@@ -13,6 +15,29 @@ n_row_groups = 5
 n_columns = 7
 chunk_size = 1 # One row group per chunk
 current_dir = os.path.dirname(os.path.realpath(__file__))
+
+class InMemoryKmsClient(pe.KmsClient):
+    def __init__(self, config):
+        super().__init__()
+        self.master_keys = config.custom_kms_conf
+
+    def wrap_key(self, key_bytes, master_key_identifier):
+        master = self.master_keys[master_key_identifier].encode()
+        padded = master * (len(key_bytes) // len(master) + 1)
+        return base64.b64encode(bytes(a ^ b for a, b in zip(key_bytes, padded[:len(key_bytes)]))).decode()
+
+    def unwrap_key(self, wrapped_key, master_key_identifier):
+        key_bytes = base64.b64decode(wrapped_key)
+        master = self.master_keys[master_key_identifier].encode()
+        padded = master * (len(key_bytes) // len(master) + 1)
+        return bytes(a ^ b for a, b in zip(key_bytes, padded[:len(key_bytes)]))
+
+crypto_factory = pe.CryptoFactory(lambda config: InMemoryKmsClient(config))
+
+def get_kms_connection_config():
+    config = pe.KmsConnectionConfig()
+    config.custom_kms_conf = {'footer_key': 'masterkey1234567', 'col_key': 'colmaster1234567'}
+    return config
 
 def get_table():
     # Generate a random 2D array of floats using NumPy
@@ -306,5 +331,65 @@ class TestPalletJack(unittest.TestCase):
             # Compare the actual output to the expected output
             self.assertEqual(index_data1, index_data2)
 
+    def test_encrypted_footer_no_key(self):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            path = os.path.join(tmpdirname, "encrypted_footer.parquet")
+            table = get_table()
+            enc_config = pe.EncryptionConfiguration(
+                footer_key='footer_key',
+                uniform_encryption=True,
+                plaintext_footer=False,
+            )
+            props = crypto_factory.file_encryption_properties(get_kms_connection_config(), enc_config)
+            pq.write_table(table, path, encryption_properties=props)
+
+            with self.assertRaises(RuntimeError) as context:
+                pj.generate_metadata_index(path)
+            self.assertIn("encrypted footer", str(context.exception))
+
+    def test_encrypted_footer_parquet(self):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            path = os.path.join(tmpdirname, "encrypted_footer.parquet")
+            table = get_table()
+            enc_config = pe.EncryptionConfiguration(
+                footer_key='footer_key',
+                uniform_encryption=True,
+                plaintext_footer=False,
+            )
+            enc_props = crypto_factory.file_encryption_properties(get_kms_connection_config(), enc_config)
+            pq.write_table(table, path, row_group_size=chunk_size, encryption_properties=enc_props)
+
+            dec_config = pe.DecryptionConfiguration(cache_lifetime=300)
+            dec_props = crypto_factory.file_decryption_properties(get_kms_connection_config(), dec_config)
+
+            index_data = pj.generate_metadata_index(path, decryption_properties=dec_props)
+            self.assertIsNotNone(index_data)
+
+            metadata = pj.read_metadata(index_data=index_data)
+            self.assertEqual(metadata.num_row_groups, n_row_groups)
+            self.assertEqual(metadata.num_columns, n_columns)
+
+
+    def test_encrypted_column_metadata_parquet(self):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            path = os.path.join(tmpdirname, "encrypted_plaintext_footer.parquet")
+            table = get_table()
+            enc_config = pe.EncryptionConfiguration(
+                footer_key='footer_key',
+                column_keys={'col_key': [f'column_{i}' for i in range(n_columns)]},
+                plaintext_footer=True,
+            )
+            props = crypto_factory.file_encryption_properties(get_kms_connection_config(), enc_config)
+            pq.write_table(table, path, row_group_size=chunk_size, encryption_properties=props)
+
+            index_data = pj.generate_metadata_index(path)
+            self.assertIsNotNone(index_data)
+
+            metadata = pj.read_metadata(index_data=index_data)
+            self.assertEqual(metadata.num_row_groups, n_row_groups)
+            self.assertEqual(metadata.num_columns, n_columns)
+
 if __name__ == '__main__':
     unittest.main()
+    # unittest.main(argv=['first-arg-is-ignored', '-k', 'TestPalletJack.test_encrypted_footer_parquet'])
+
